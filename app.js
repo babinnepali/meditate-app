7316ae5e396f405198eeab16a527ee16c31c3d4c
babinnepali <babinnepalibtl@gmail.com>
2020-08-12 14:07:54 +0545

    Added moving circle length

---
+++ app.js
@@ -0,0 +1,15 @@
+const app = () => {
+    const song = document.querySelector('.song');
+    const play = document.querySelector('.play');
+    const outline = document.querySelector('.moving-outline circle');
+    const video = document.querySelector('.vid-container video');
+
+    //Sounds
+    const sounds = document.querySelector('.sound-picker button');
+    //Time Display
+    const timeDisplay = document.querySelector('.time-display');
+    //Get the Length of the outline
+    const outlineLength = outline.getTotalLength();
+    console.log(outlineLength);
+}
+app();
